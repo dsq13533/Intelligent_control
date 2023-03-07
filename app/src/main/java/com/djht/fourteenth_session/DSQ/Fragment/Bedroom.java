22e778a762688e59db74f6f2ef227e33e4581429
dsqzqm <2564305991@qq.com>
2023-03-07 18:45:32 +0800

    light连接数据库

--- app/src/main/java/com/djht/fourteenth_session/DSQ/Fragment/Bedroom.java
+++ app/src/main/java/com/djht/fourteenth_session/DSQ/Fragment/Bedroom.java
@@ -1,17 +1,28 @@
 package com.djht.fourteenth_session.DSQ.Fragment;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.djht.fourteenth_session.DataBase.DBService;
+import com.djht.fourteenth_session.DataBase.Version;
 import com.djht.fourteenth_session.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Bedroom extends Fragment {
+    private ListView bedroom_ListView;
 
     @Nullable
     @Override
@@ -23,5 +34,34 @@ public class Bedroom extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        bedroom_ListView=getActivity().findViewById(R.id.bedroom_ListView);
+
+        DBService data_operate=new DBService(getContext(),"diary.db",null, Version.DB_Version);//最后一个参数是数据库版本
+        List<Map<String,Object>> listitem=new ArrayList<>();
+        Cursor data = data_operate.getReadableDatabase().query("light", null, null, null, null, null, null);
+            while(data.moveToNext())
+            {
+                if(data.getString(3).equals("卧室")){
+                    Map<String,Object> map=new HashMap<>();
+                    map.put("light_number",data.getString(1));
+                    map.put("light_name",data.getString(2));
+                    if(data.getString(4).equals("0")){
+                        map.put("light_state","关");
+                    }else {
+                        map.put("light_state","开");
+                    }
+                    listitem.add(map);
+                }
+            }
+            SimpleAdapter adapter=new SimpleAdapter(getContext(),
+            listitem,
+            R.layout.light_layout,
+            new String[]{"light_number","light_name","light_state"},
+            new int[]{R.id.light_number,R.id.light_name,R.id.light_state});
+            bedroom_ListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/djht/fourteenth_session/DataBase/DBService.java
+++ app/src/main/java/com/djht/fourteenth_session/DataBase/DBService.java
@@ -5,34 +5,32 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.djht.fourteenth_session.DSQ.DataBase.DBService;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class DBservice extends SQLiteOpenHelper {
+public class DBService extends SQLiteOpenHelper {
 
     //智能灯【灯编号 | 灯名字 | 灯位置 | 开启状态】
-    String LIGHT="create table light(id integer primary key autoincrement,number,name,location,state);";
+    String LIGHT="create table light(id integer primary key autoincrement,number,name,location,state,ip);";
     String LIADD="insert into light(number,name,location,state) values('001','主灯','卧室','0');";
 
     //智能门锁【锁编号 | 锁密码 | 锁状态】
-    String LOCK="create table lock(id integer primary key autoincrement,number,password,state);";
+    String LOCK="create table lock(id integer primary key autoincrement,number,password,state,ip);";
     String LOADD="insert into lock(number,password,state) values('001','123456','0')";
 
     //智能空调【空调编号 | 空调名字 | 空调位置 | 空调温度 | 空调状态】
-    String AIR="create table air(id integer primary key autoincrement,number,name,location,temperature,state);";
+    String AIR="create table air(id integer primary key autoincrement,number,name,location,temperature,state,ip);";
     String AIADD="insert into air(number,name,location,temperature,state) values('001','海尔挂墙式','客厅','20','0');";
 
     //智能热水器【热水器编号 | 热水器名字 | 热水器温度 | 热水器状态】
-    String HEATER="create table heater(id integer primary key autoincrement,number,name,temperature,state);";
+    String HEATER="create table heater(id integer primary key autoincrement,number,name,temperature,state,ip);";
     String HEADD="insert into heater(number,name,temperature,state) values('001','一楼','20','0');";
 
 
 
     //创建表初始化
-    public DBservice(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public DBService(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
